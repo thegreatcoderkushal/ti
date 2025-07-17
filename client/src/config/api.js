c5d64de554015b2a050bb43e39aef569c5dc2f87
thegreatcoderkushal <kushalajwani8@gmail.com>
2025-07-17 14:31:50 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,4 @@
 // Base API URL configuration
-const API_BASE_URL = 'https://ecd9c64c94ff.ngrok-free.app ';
+const API_BASE_URL = 'https://ecd9c64c94ff.ngrok-free.app';
 //const API_BASE_URL = 'http://localhost:8000';
 export { API_BASE_URL };
